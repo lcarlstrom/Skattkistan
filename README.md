c66ebee6f8b63a18e28ab300b8951f8d434c5177
lcarlstrom <lukaskurtcarlstrom@gmail.com>
2025-12-15 20:29:43 +0100

    Skattkistan ver 1.1 + readme
    Alla lösenord visas nu initialt som asterisker med en tillkommen toggle knapp för att visa/åter-dölja lösenorden.

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ sedan visa detta genererade lösenord.
 Skattkistan körs genom att starta skriptet varpå användaren får ta del av ett grafiskt interface med en input-låda och en generations-knapp.
 I input-lådan bör ett integer värde matas in baserat på önskad lösenordslängd.
 Efter input har fyllts klickar användaren på generations-knappen och får lösenord i den högre spalten.
-
+Dessa lösenord syns default som asterisker med en knapp vid sidan för att visa/dölja lösenordet.
--- skattkistan.py
+++ skattkistan.py
@@ -14,6 +14,12 @@ root.geometry("800x500")
 group = Frame(root, bg="#f5f5f5", bd=4, relief=RAISED)          # Frame 1 som ska inkludera längd-definitionen samt
 group.place(relx=0.03, rely=0.1, relheight=0.8, relwidth=0.4)     # lösen-generationswidgeten. 
 
+separate = ttk.Separator(root, orient="vertical")           # Visuell separator för att skilja på frame 1 och 2
+separate.place(relx=0.47, rely=0, relwidth=0.2, relheight=1)
+
+group2 = Frame(root, bg="#f5f5f5", bd=4, relief=SUNKEN)         # Frame två där skapade lösenord ska sparas
+group2.place(relx=0.51, rely=0.1, relheight=0.8, relwidth=0.4)
+
 txt_label = Label(group, text = "Längd: ")   # Definierar att det efterfrågas "Längd" vid input-fältet
 txt_label.place(relx=0.35, rely=0.28, relwidth=0.3)
 
@@ -26,23 +32,36 @@ length.trace_add("write", save_length)      # Varje gång entryfältet "längd" 
 txt = Entry(group, textvariable=length)      # "save_length" för att spara ned det senaste värdet.
 txt.place(relx=0.35, rely=0.35, relwidth=0.3)
 
-
+rowcount = 0                # Adderas med 1 efter varje lösenordsgeneration för att lösenorden ska ordnas efter varandra i GUI:n
 
 def passgen():
+        global rowcount
         length = int(save_length())
         chars = string.ascii_letters + string.digits + string.punctuation # Alla karaktärer som vanligtvis är tillåtna i lösenord
         password = "".join(secrets.choice(chars) for i in range(length)) # Ta ett slumpat urval från "chars" "length" antal gånger
-        pwd_label = Label(group2, text = password)      # Lägg till lösenordet i GUI:n
-        pwd_label.pack()
+        pwd_label = Label(group2, text = len(password) * "*")      # Lägg till lösenordet i GUI:n i asterisk-format
+        pwd_label.grid(column=0, row=rowcount)
+        pwd_labels = []                       # Skapa en lista av alla lösenords widgets
+        pwd_labels.append(pwd_label)
+        def toggle_password():                  # Funktion för att visa lösenordet när användaren klickar på "?"
+            for pwd_lab in pwd_labels:        # För varje individuell widget i listan av widgets  
+                if pwd_lab.cget("text").startswith("*"):    # Om texten i widgeten börjar med asterisker
+                    pwd_lab.config(text=password) # Gör om asteriskerna till det faktiska lösenordet
+                else:                             # Men om det inte är asterisker  
+                    pwd_lab.config(text=len(password) * "*")    # Gör om det till asterisker
+        showbutton = ttk.Button(group2, text="?", command=toggle_password) # Lägg till knapp för att visa lösenordet
+        showbutton.grid(column=1, row = rowcount)
+        rowcount += 1
+        return password
+     
 
 buttongen = ttk.Button(group, text="Generera ett lösenord", command=passgen) # Knapp för att generera lösenord
 buttongen.place(relx=0.30, rely=0.43, relwidth=0.4)
 
-separate = ttk.Separator(root, orient="vertical")           # Visuell separator för att skilja på frame 1 och 2
-separate.place(relx=0.47, rely=0, relwidth=0.2, relheight=1)
 
-group2 = Frame(root, bg="#f5f5f5", bd=4, relief=SUNKEN)     # Frame två där skapade lösenord ska sparas
-group2.place(relx=0.51, rely=0.1, relheight=0.8, relwidth=0.4)
+# Lägg till knapp för att kopiera lösenordet
+
+# Lägg till knapp för att ta bort lösenordet
 
 
 
